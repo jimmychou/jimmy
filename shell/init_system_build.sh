147bd35b3f16dafa716202acffa161e546331f61
Build@CentOS6 <Build@CentOS6>
2015-04-27 18:18:32 +0800

    php config is different in vary version

--- shell/init_system_build.sh
+++ shell/init_system_build.sh
@@ -124,9 +124,9 @@ for i in $*; do
 		fi
 			#	否则会报错：failed to open error_log (/var/log/php-fpm/error.log): No such file or directory
 		#sudo groupadd apache && sudo useradd -M -g apache apache	#	否则启动时会报错：cannot get uid for user 'apache'
-		#if [ ! -d "/etc/php.d" ]; then
+		if [ ! -d "/etc/php.d" ]; then
 			sudo mkdir /etc/php.d
-		#fi
+		fi
 		#sudo cp ~/workspace/jimmy/os/centos/build/build_as_system/conf/php/php.d/* /etc/php.d/
 		sudo cp ~/workspace/jimmy/os/centos/build/build_as_system/init.d/php-fpm /etc/init.d/
 		if [[ -z `getsebool httpd_read_user_content | grep '\--> on'` ]]; then
