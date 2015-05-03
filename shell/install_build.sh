ca715b1ec0a887ab717c0d31a0062641485da4c1
Build@CentOS5 <Build@CentOS5>
2015-05-03 22:11:50 +0800

    add install_buils script

---
+++ shell/install_build.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+if [ $# -le 0 ]; then
+	echo You need specify one parameter at least!
+	exit
+fi
+source ./release.sh
+SOFTWARE=~/software
+for i in $*; do 
+	if [[ $i =~ "gperftools" ]]; then
+		#	Nginx	的	google_perftools	模块
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION.make_done.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+	elif [[ $i =~ "nginx" ]]; then
+		#	Nginx
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION.make_done.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+	elif [[ $i =~ "httpd" ]]; then
+		#	Httpd
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION.make_done.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+	elif [[ $i =~ "mysql" ]]; then
+		#	MySQL
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION.make_done.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+	elif [[ $i =~ "php-" || $i == 'php' ]]; then
+		#	避免	phpMyAdmin	走此分支
+		#	PHP
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION.make_done.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+	elif [[ $i =~ "memcached" ]]; then
+		#	Memcached
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION.make_done.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+	elif [[ $i =~ "phpMyAdmin" ]]; then
+		#	phpMyAdmin
+		SOFT_NAME=`echo $i | awk -F "-" '{print $1}'`
+		SOFT_VERSION=`echo $i | awk -F "-" '{print $2}'`
+		if [[ -z $SOFT_VERSION ]]; then
+			source ./get_soft_version.sh
+		fi
+		SOFT_ZIP=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION-all-languages.zip
+		SOFT_INSTALL_DIR=$SOFTWARE/$SOFT_NAME-$SOFT_VERSION-all-languages
+		if [ ! -d $SOFT_INSTALL_DIR ]; then
+			echo 安装文件夹不存在，需要解压安装压缩包
+			if [ ! -f $SOFT_ZIP ]; then
+				echo 安装压缩包不存在
+				exit
+			else
+				echo 开始解压安装压缩包
+				cd $SOFTWARE && unzip $SOFT_ZIP
+			fi
+		fi
+		if [[ $PrimaryVersion == "5" ]]; then
+			SOFT_LOWER=`echo $SOFT_NAME | tr '[A-Z]' '[a-z]'`
+				#       CentOS5 的      bash    版本为3，declare        命令不支持大小写转换，需要      bash4   支持
+		else
+			declare -l SOFT_LOWER=$SOFT_NAME
+		fi
+		if [ ! -d ~/workspace/$SOFT_NAME-$SOFT_VERSION-all-languages ]; then
+			cp -r $SOFT_INSTALL_DIR ~/workspace/
+		fi
+		ln -sf ~/workspace/$SOFT_NAME-$SOFT_VERSION-all-languages ~/workspace/$SOFT_LOWER
+	fi
+done
